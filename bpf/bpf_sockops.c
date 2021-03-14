86197cdf61c8f28ee1a074a4b2370c5b4ea6b1a0
I-mpossible <582932205@qq.com>
2021-03-14 17:18:27 +0800

    testing changing dport

--- bpf/bpf_sockops.c
+++ bpf/bpf_sockops.c
@@ -29,9 +29,9 @@ void bpf_sock_ops_ipv4(struct bpf_sock_ops *skops)
     int ret;
 
     printk("\nskops dport is %d\n", bpf_ntohl(skops->remote_port));
-    skops->remote_port = 1001
+    skops->remote_port = 1001;
     printk("\nskops dport changed %d\n", bpf_ntohl(skops->remote_port));
-    
+
     extract_key4_from_ops(skops, &key);
 
     ret = sock_hash_update(skops, &sock_ops_map, &key, BPF_NOEXIST);
