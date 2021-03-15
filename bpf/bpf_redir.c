f2c14bb3560aa5baacda073ccfc427a0746d34f3
I-mpossible <582932205@qq.com>
2021-03-15 11:31:37 +0800

    testing changing dport

--- bpf/bpf_redir.c
+++ bpf/bpf_redir.c
@@ -22,7 +22,7 @@ int bpf_redir(struct sk_msg_md *msg)
 {
     struct sock_key key = {};
     extract_key4_from_msg(msg, &key);
-    printf("msg dport: %d\n", key.dport);
+    printf("msg dport: %d\n", &key->dport);
     // if (key.dport == 1000){
     	// key.dport = 1001;
     	msg_redirect_hash(msg, &sock_ops_map, &key, BPF_F_INGRESS);
